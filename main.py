f3a23977d8c9bef9007286d6e7224fea66e739d3
CoddingNinja <ahmedhaseeb93338@gmail.com>
2024-09-27 19:00:56 +0500

    get price method implemented

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class Product:
       self.price = price
 
   def get_price(self, quantity):
-      pass
+      return self.price if 0 < quantity > 10 else (self.price * 0.1 if 10 <= quantity > 99 else self.price * 0.2)
 
   def make_purchase(self, quantity):
       pass
