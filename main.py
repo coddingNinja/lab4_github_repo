637f11779f9aba0846125340f6ea747d4d8f23af
CoddingNinja <ahmedhaseeb93338@gmail.com>
2024-09-27 20:17:33 +0500

    resolved a bug in get_price

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ class Product:
       self.price = price
 
   def get_price(self, quantity):
-      return self.price if 0 < quantity > 10 else (self.price * 0.1 if 10 <= quantity > 99 else self.price * 0.2)
+      return self.price * quantity if 0 < quantity > 10 else (self.price * 0.1 * quantity if 10 <= quantity > 99 else self.price * 0.2 * quantity)
 
   def make_purchase(self, quantity):
+      
       pass
