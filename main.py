9f9c32a88b504f04a90993b10843d5e1c577aead
CoddingNinja <ahmedhaseeb93338@gmail.com>
2024-09-27 18:37:29 +0500

    commit in main to check the git hub connection

---
+++ main.py
@@ -0,0 +1,11 @@
+class Product:
+  def __init__(self, name, amount, price):
+      self.name = name
+      self.amount = amount
+      self.price = price
+
+  def get_price(self, quantity):
+      pass
+
+  def make_purchase(self, quantity):
+      pass
